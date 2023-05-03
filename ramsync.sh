f8a3ba3bddfbae137ad18d507d1fe98dece08f2c
argon3x <r0us3.me17@gmail.com>
2023-05-02 22:58:53 -0600

    ramsync version 2.0

--- ramsync.sh
+++ ramsync.sh
@@ -1,62 +1,74 @@
 #!/bin/bash
 
-# - COLORS - #
-green="\e[01;32m"; red="\e[01;31m"
-blue="\e[01;34m"; yellow="\e[01;33m"
-purple="\e[01;35m"; end="\e[00m"
+### By: Argon3x
+### Supported: Debian Based Systems
+### Version: 2.0
 
-function CTRL_C(){
-  exit 0
-  tput cnorm
-}
-function terminated(){
-  exit 0
-  tput cnorm
-}
-trap CTRL_C INT
-trap terminated SIGTERM
+# Colors
+red="\e[01;31m"; green="\e[01;32m"; blue="\e[01;34m"
+purple="\e[01;35m"; yellow="\e[01;33m"; end="\e[00m"
 
-function freeing(){
-  i=1
-  while [[ ${i} -le 3 ]]; do
-    echo -e "${yellow}...${end}\c"
-    `sudo sync > /dev/null 2>&1 && sudo sysctl -w vm.drop_caches=3 > /dev/null 2>&1`
-    if [[ ${?} -ne 0 ]]; then
-      echo -e "${red}--------------------${end}"
-      echo -e "${red}  An Error Ocurred ${end}"
-      echo -e "${red}--------------------${end}"
-      terminated
+# Context box
+box="${purple}[${green}+${purple}]${end}"
+
+# Signal Interrupt and Error
+interrupt_handler(){
+  echo -e "\n${blue}>>>> ${red}Process Canceled ${blue}<<<<${end}\n"
+  tput cnorm
+  exit 1
+}
+
+error_handler(){
+  local type_error="$1"
+  echo -e "${purple}Script Error${end}:{ \n\t${red}${type_error}${end}\n}"
+  tput cnorm
+  exit 1
+}
+
+# Call the signals
+trap interrupt_handler SIGINT
+trap error_handler SIGTERM
+
+# Clear Ram Memory Cache.
+clean_cache_ram(){
+  # Synchronize File System
+  echo -e "${box} ${yellow}Sinchronizing file system${end}............\c"; sleep 0.5
+  command sudo sync
+  if [[ $? -eq 0 ]]; then
+    echo -e "${green} done ${end}"
+  else
+    error_handler "An Error Ocurred While Sinchronizing The File System"
+  fi
+
+  # Flushing the kernel page cache and the inode and dentry object cache.
+  echo -e "${box} ${yellow}Clearing the cache${end}\c";
+  for c in $(seq 1 3); do
+    command sudo echo $c > /proc/sys/vm/drop_caches
+    if [[ $? -eq 0 ]]; then
+      echo -e "....\c"
+      if [[ ${c} -eq 3 ]]; then
+        echo -e "${green} done ${end}"
+      fi
+    else
+      echo -e "${red} failed ${end}"
+      error_handler "An Error Ocurred While Clearing The Cache"
     fi
-    sleep 0.5
-    i=$[${i}+1]
   done
-  echo
 }
 
-function currentStat(){
-  state_free=$(free --human --giga | grep 'Mem:' | awk '{print $4}')
-  state_used=$(free --human --giga | grep 'Mem:' | awk '{print $3}')
-  sleep 1  
-  echo -e "${purple}FREE ${blue}-> ${green}${state_free} ${purple}USED ${blue}-> ${red}${state_used}"
-}
-
-
-function main(){
-  tput civis echo -e "${blue}Checking ${red}RAM ${blue}Memory${yellow}.......${end}"; sleep 1
-  echo -e "${blue}Current State ${end}\c"
-  currentStat
-  echo -e "${blue}Freeing ${red}RAM ${blue}Memory${end}\c"; freeing
-  echo -e "${blue}Current State ${end}\c"
-  currentStat
-  tput cnorm
-}
-
-
-clear
+# Checking Execution As Root
 if [[ $(id -u) -eq 0 ]]; then
-  main
+  tput civis && clear
+  echo -e "${box} ${yellow}Freeing up RAM${end}..........."; sleep 1
+  # Clean Cache Ram Function.
+  clean_cache_ram
+
+  tput cnorm
 else
-  echo -e "${red}----------------------${end}"
-  echo -e "${red} use: ${green}sudo ${0}${end}"
-  echo -e "${red}----------------------${end}"
+  error_handler "use: ${green}sudo ./${0##*/}${end}"
 fi
+
+# Clearing the function and variables
+sleep 1
+unset -f clean_cache_ram
+unset red green blue purple yellow end
